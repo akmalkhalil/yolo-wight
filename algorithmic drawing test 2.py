24645c025ea2ec61237c558f7c91e14d657153d7
Akmal Khali <silenttechy@users.noreply.github.com>
2014-08-21 16:44:15 +0100

    Update algorithmic drawing test 2.py

--- algorithmic drawing test 2.py
+++ algorithmic drawing test 2.py
@@ -20,8 +20,10 @@ colour = {
 }
 count = 0
 running = True
+sides = 10
+bg = 'black'
 while running:
-    windowSurf.fill(colour['black'])
+    windowSurf.fill(colour[bg])
 ##    pygame.draw.circle(windowSurf,colour['red'], (int(windowW/2),int(windowH/2)), int(20 +10*math.sin(count)), 1)
 ##    pygame.draw.circle(windowSurf, colour['blue'], (int(windowW/2+10*math.sin(count)), int(windowH/2+10*math.cos(count))), 20, 1)
     
@@ -31,8 +33,12 @@ while running:
 ##        pygame.draw.line(windowSurf, colour['green'], (windowW/2+40*math.sin((i/10)*math.pi), windowH/2+40*math.cos((i/10)*math.pi)), (windowW/2+40*math.sin((i/10+1)*math.pi), windowH/2+40*math.cos((i/10+1)*math.pi) ) )
 
     for i in range(10):
-        pygame.draw.circle(windowSurf, colour['magenta'], (int(windowW/2 + math.sin(i/10 * 2 * math.pi * count)*i*10),int(windowH/2 + math.cos(i/10 * 2 * math.pi * count)*i*10)), 10,1)
+        pygame.draw.circle(windowSurf, colour['magenta'], (int(windowW/2 + math.sin(i/10 * 2 * math.pi * count)*i*10),int(windowH/2 + math.cos(i/10 * 2 * math.pi * count)*i*10)), 15,1)
+
     
+##    for i in range(sides):
+##        pygame.draw.line(windowSurf, colour['cyan'], (int(windowW/2 + math.sin( i/sides * 2 * math.pi) * 40), int(windowH/2 + math.cos( i/sides * 2 * math.pi)*40)),(int(windowW/2 + math.sin( (i+1)/sides * 2 * math.pi) * 40), int(windowH/2 + math.cos( (i+1)/sides * 2 * math.pi)*40)) )
+    #now i wanna do summat with this to make it move
     pygame.display.update()
     fpsClock.tick(30)
     
@@ -43,5 +49,12 @@ while running:
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 pygame.event.post(pygame.event.Event(QUIT))
+        elif event.type == MOUSEBUTTONDOWN:
+            if bg == 'black':
+                bg = 'white'
+            elif bg == 'white':
+                bg = 'black'
+    #print(count)
                 
 print('buh bye')
+print('count =',count)
